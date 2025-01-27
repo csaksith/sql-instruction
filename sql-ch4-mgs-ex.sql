b521d0c6c3d67fa9c244db54fa5455c548ee2e80
csaksith <saksitca@mail.uc.edu>
2025-01-27 10:50:52 -0500

    added more demos from ch 4 my guitar shop

--- sql-ch4-mgs-ex.sql
+++ sql-ch4-mgs-ex.sql
@@ -13,5 +13,19 @@ SELECT *
 SELECT FirstName, LastName, Line1 AS Address,City, State, ZipCode
 	FROM Customers C
 	JOIN Addresses A ON C.ShippingAddressID=A.AddressID
+	AND C.CustomerID=A.CustomerID;
+	--customerID has to meet shipping address,
+
+
+--#5 Write a SELECT statement that returns the ProductName and ListPrice columns from the Products table.
+--Return one row for each product that has the same list price as another product.
+--(Hint: Use a self-join to check that the ProductID columns aren’t equal but the ListPrice column is equal.)
+--Sort the result set by ProductName
+
+SELECT P1.ProductName, P2.ProductName,P1.ListPrice
+	FROM Products P1
+	JOIN Products P2 ON P1.ListPrice=P2.ListPrice
+	WHERE P1.ProductID<>P2.ProductID
+	ORDER BY ListPrice;
 
 
